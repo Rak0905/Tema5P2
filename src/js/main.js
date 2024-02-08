1e10383e2228384c3eed8207acdc5152d0a717e1
raquel <raqueloles@hotmail.com>
2024-02-08 18:50:12 +0100

    Ultimos detalles

--- src/js/main.js
+++ src/js/main.js
@@ -550,6 +550,50 @@ deassignAllergenToDish(dish, ...allergens) {
 
   return this;
 }
+assignMenuToRestaurant(menu, restaurant) {
+  if (!(menu instanceof Menu)) {
+    throw new Error("El parámetro 'menu' no es una instancia válida de Menu");
+  }
+
+  if (!(restaurant instanceof Restaurant)) {
+    throw new Error("El parámetro 'restaurant' no es una instancia válida de Restaurant");
+  }
+
+  // Comprobar si el restaurante ya tiene un menú asignado
+  if (this._restaurants.has(restaurant.getName())) {
+    const storedRestaurant = this._restaurants.get(restaurant.getName());
+
+    if (storedRestaurant.menu) {
+      throw new Error("El restaurante ya tiene un menú asignado");
+    }
+  }
+
+  // Asignar el menú al restaurante
+  restaurant.menu = menu;
+  return this;
+}
+
+// Método para desasignar un menú de un restaurante
+deassignMenuFromRestaurant(restaurant) {
+  if (!(restaurant instanceof Restaurant)) {
+    throw new Error("El parámetro 'restaurant' no es una instancia válida de Restaurant");
+  }
+
+  // Comprobar si el restaurante tiene un menú asignado
+  if (this._restaurants.has(restaurant.getName())) {
+    const storedRestaurant = this._restaurants.get(restaurant.getName());
+
+    if (!storedRestaurant.menu) {
+      throw new Error("El restaurante no tiene un menú asignado");
+    }
+
+    // Desasignar el menú del restaurante
+    storedRestaurant.menu = null;
+    return this;
+  } else {
+    throw new Error("El restaurante no existe");
+  }
+}
 
 
  
@@ -814,34 +858,51 @@ function initializePage() {
   const centralZone = document.getElementById('central-zone');
   const centralZone2 = document.getElementById('central-zone2');
   
-  
-  // ... (Cargar categorías, código existente)
+  function showCategories() {
+    const centralZone = document.getElementById('central-zone');
 
-  // Función para mostrar las categorías
-function showCategories() {
-  // Limpiar el contenido del 'central-zone'
-  centralZone.innerHTML = '';
+    // Limpiar el contenido del 'central-zone'
+    centralZone.innerHTML = '';
 
-  // Iterar sobre las categorías y mostrar los enlaces
-  restaurantsManager.categories.forEach(category => {
-    const categoryLink = document.createElement('a');
-    categoryLink.href = 'javascript:void(0)';
-    categoryLink.textContent = category.getName();
-    categoryLink.addEventListener('click', () => showCategoryDetails(category));
+    // Agregar encabezado h3 con "Lista de Categorías"
+    const heading = document.createElement('h3');
+    heading.textContent = 'Lista de Categorías';
+    centralZone.appendChild(heading);
 
-    centralZone.appendChild(categoryLink);
+    // Iterar sobre las categorías y mostrar los enlaces
+    restaurantsManager.categories.forEach(category => {
+        const categoryLink = document.createElement('a');
+        categoryLink.href = 'javascript:void(0)';
+        categoryLink.textContent = category.getName();
 
-    // Añadir un salto de línea después de cada enlace de categoría
-    centralZone.appendChild(document.createElement('br'));
-  });
-  
+        categoryLink.addEventListener('click', () => {
+            // Ocultar las zonas con la clase 'zone' excepto la zona central
+            const zonesToHide = document.querySelectorAll('.zone:not(#central-zone)');
+            zonesToHide.forEach(zone => zone.style.display = 'none');
+            
+            showCategoryDetails(category);
+            
+        });
 
+        centralZone.appendChild(categoryLink);
+
+        // Añadir un salto de línea después de cada enlace de categoría
+        centralZone.appendChild(document.createElement('br'));
+    });
 }
+
+
+  
 
   // Mostrar tres platos aleatorios debajo de las categorías
   function showRandomDishes() {
     const centralZone2 = document.getElementById('central-zone2');
     centralZone2.innerHTML = '';
+  
+    // Agregar un encabezado h4 como prueba
+    const heading = document.createElement('h4');
+    heading.textContent = 'Prueba un plato de nuestra selección';
+    centralZone2.appendChild(heading);
   
     const randomDishes = restaurantsManager.getRandomDishes(3);
     randomDishes.forEach(dishData => {
@@ -887,6 +948,11 @@ function mostrarNombresPlatosYMenuEnHTML() {
   // Limpia el contenido actual del elemento 'menu'
   menuElement.innerHTML = '';
 
+    // Agregar un encabezado h3 
+    const heading = document.createElement('h3');
+    heading.textContent = 'Nuestros menus';
+    menuElement.appendChild(heading);
+  
   // Itera sobre los menús
   restaurantsManager.menus.forEach(menuData => {
     const menu = menuData.menu;
@@ -908,6 +974,9 @@ function mostrarNombresPlatosYMenuEnHTML() {
 
       // Crear un div adicional para la información adicional del plato (inicialmente oculto)
       const additionalInfoDiv = document.createElement('div');
+       // Agregar la clase "plato" al elemento 'div' del plato
+       dishDiv.classList.add('plato');
+       
       additionalInfoDiv.style.display = 'none';
       additionalInfoDiv.innerHTML = `&nbsp;&nbsp;&nbsp;<strong>Descripción:</strong> ${dish.getDescription()}<br>`;
 
@@ -936,6 +1005,53 @@ function mostrarNombresPlatosYMenuEnHTML() {
 mostrarNombresPlatosYMenuEnHTML();
 
 
+function crearMenuDesplegableRestaurantes() {
+  // Obtén una referencia al elemento HTML donde deseas agregar el menú desplegable
+  const selectElement = document.getElementById('restaurant-select');
+
+  // Limpia el contenido actual del elemento
+  selectElement.innerHTML = '';
+
+  // Agrega una opción predeterminada
+  const defaultOption = document.createElement('option');
+  defaultOption.text = 'Selecciona un restaurante';
+  defaultOption.disabled = true;
+  defaultOption.selected = true;
+  selectElement.appendChild(defaultOption);
+
+  // Itera sobre los restaurantes
+  restaurantsManager.restaurants.forEach(restaurant => {
+    // Crea una opción de menú para cada restaurante
+    const option = document.createElement('option');
+    option.value = restaurant.getName();
+    option.text = restaurant.getName();
+    selectElement.appendChild(option);
+  });
+
+  // Agrega un evento de cambio al menú desplegable
+  selectElement.addEventListener('change', (event) => {
+    // Obtiene el restaurante seleccionado
+    const selectedRestaurantName = event.target.value;
+
+    // Encuentra el restaurante correspondiente en la lista de restaurantes
+    const selectedRestaurant = restaurantsManager.restaurants.find(restaurant => restaurant.getName() === selectedRestaurantName);
+
+    // Muestra la información del restaurante seleccionado
+    mostrarInformacionRestaurante(selectedRestaurant);
+  });
+}
+
+
+function mostrarInformacionRestaurante(restaurant) {
+  // Muestra la información del restaurante en algún lugar de la interfaz de usuario
+  const infoElement = document.getElementById('restaurant-info');
+  infoElement.innerHTML = `
+    <h3>${restaurant.getName()}</h3>
+    <p>Descripción: ${restaurant.getDescription()}</p>
+    <p>Ubicación: ${restaurant.getLocation()}</p>
+  `;
+}
+
 
 
 function showIngredients(dish) {
@@ -947,53 +1063,9 @@ function showIngredients(dish) {
   // Por ejemplo, puedes añadirlo al final del cuerpo del documento
   document.body.appendChild(ingredientsElement);
 }
-function showAllergensWithDishes() {
-  const allergensList = document.getElementById('allergens-list');
-
-  // Limpiar la lista de alérgenos
-  allergensList.innerHTML = '';
-
-  // Iterar sobre cada alérgeno
-  restaurantsManager.allergens.forEach(allergen => {
-    // Crear un nuevo elemento de lista para el alérgeno
-    const allergenItem = document.createElement('li');
-    allergenItem.textContent = `${allergen.getName()}:`;
-
-    // Obtener los platos que contienen el alérgeno
-    const dishesWithAllergen = restaurantsManager.getDishesWithAllergen(allergen);
-
-    if (dishesWithAllergen.length > 0) {
-      // Crear una lista ordenada para los platos
-      const dishesList = document.createElement('ol');
-
-      // Iterar sobre cada plato y agregarlo a la lista
-      dishesWithAllergen.forEach(dish => {
-        const dishItem = document.createElement('li');
-        dishItem.textContent = dish.getName();
-        dishesList.appendChild(dishItem);
-      });
-
-      // Agregar la lista de platos al elemento de alérgeno
-      allergenItem.appendChild(dishesList);
-
-      // Agregar evento de clic para toggle la visibilidad de los platos
-      allergenItem.addEventListener('click', () => {
-        dishesList.style.display = dishesList.style.display === 'none' ? 'block' : 'none';
-      });
-    } else {
-      // Si no hay platos con el alérgeno, agregar un mensaje
-      const noDishesMessage = document.createElement('p');
-      noDishesMessage.textContent = 'Ningún plato contiene este alérgeno.';
-      allergenItem.appendChild(noDishesMessage);
-    }
-
-    // Agregar el elemento de alérgeno a la lista de alérgenos
-    allergensList.appendChild(allergenItem);
-  });
-}
 
 
-// ...
+
 
 // Llamar a la función para mostrar la lista de alérgenos y los platos
 showAllergensWithDishes();
@@ -1001,8 +1073,8 @@ showAllergensWithDishes();
 showRandomDishes();
 
 showCategories();
+crearMenuDesplegableRestaurantes();
 
-// ...
 
 // En la función initializePage, después de crear enlaces para cada categoría
 // Agregar el siguiente código para mostrar platos aleatorios al inicio
@@ -1013,6 +1085,11 @@ function showCategoryDetails(category) {
 
   // Limpiar el contenido del 'central-zone'
   centralZone.innerHTML = '';
+
+  // Agregar el nombre de la categoría como encabezado
+  const categoryHeading = document.createElement('h3');
+  categoryHeading.textContent = category.getName();
+  centralZone.appendChild(categoryHeading);
 
   // Obtener o crear el elemento para mostrar los ingredientes
   let ingredientsElement = document.getElementById('ingredients');
@@ -1060,11 +1137,88 @@ function showCategoryDetails(category) {
 
   // Añadir el botón de volver a categorías
   const backButton = document.createElement('button');
+  backButton.id="btn_back"
   backButton.textContent = 'Volver a Categorías';
-  backButton.addEventListener('click', () => showCategories());
+  backButton.addEventListener('click', () => {
+    showCategories();
+    // Hacer visible la clase 'zone' al hacer clic en el botón
+    const zones = document.querySelectorAll('.zone');
+    zones.forEach(zone => {
+      zone.style.display = 'block';
+    });
+  });
   centralZone.appendChild(backButton);
 }
 }
+function showAllergensWithDishes() {
+  const allergensList = document.getElementById('allergens-list');
+
+  // Limpiar la lista de alérgenos
+  allergensList.innerHTML = '';
+
+  // Agregar encabezado h3 con "Lista de Alergenos"
+  const heading = document.createElement('h3');
+  heading.textContent = 'Lista de Alergenos';
+  allergensList.appendChild(heading);
+
+  // Iterar sobre cada alérgeno
+  restaurantsManager.allergens.forEach(allergen => {
+    // Crear un nuevo elemento de lista para el alérgeno
+    const allergenItem = document.createElement('li');
+    allergenItem.textContent = `${allergen.getName()}:`;
+
+    // Obtener los platos que contienen el alérgeno
+    const dishesWithAllergen = restaurantsManager.getDishesWithAllergen(allergen);
+
+    if (dishesWithAllergen.length > 0) {
+      // Crear una lista ordenada para los platos
+      const dishesList = document.createElement('ol');
+
+      // Iterar sobre cada plato y agregarlo a la lista
+      dishesWithAllergen.forEach(dish => {
+        const dishItem = document.createElement('li');
+        dishItem.textContent = dish.getName();
+
+        // Crear un contenedor para los ingredientes y hacerlo invisible por defecto
+        const ingredientsContainer = document.createElement('ul');
+        ingredientsContainer.style.display = 'none';
+
+        // Iterar sobre cada ingrediente del plato y agregarlo al contenedor como elementos de lista
+        dish.getIngredients().forEach(ingredient => {
+          const ingredientItem = document.createElement('li');
+          ingredientItem.textContent = ingredient;
+          ingredientsContainer.appendChild(ingredientItem);
+        });
+
+        // Agregar el contenedor de ingredientes al elemento de plato
+        dishItem.appendChild(ingredientsContainer);
+
+        // Agregar evento de clic para toggle la visibilidad de los ingredientes
+        dishItem.addEventListener('click', () => {
+          ingredientsContainer.style.display = ingredientsContainer.style.display === 'none' ? 'block' : 'none';
+        });
+
+        // Agregar la clase "plato" al elemento de plato
+        dishItem.classList.add('plato');
+
+        dishesList.appendChild(dishItem);
+      });
+
+      // Agregar la lista de platos al elemento de alérgeno
+      allergenItem.appendChild(dishesList);
+    } else {
+      // Si no hay platos con el alérgeno, agregar un mensaje
+      const noDishesMessage = document.createElement('p');
+      noDishesMessage.textContent = 'Ningún plato contiene este alérgeno.';
+      allergenItem.appendChild(noDishesMessage);
+    }
+
+    // Agregar el elemento de alérgeno a la lista de alérgenos
+    allergensList.appendChild(allergenItem);
+  });
+}
+
+
 
 // Mostrar detalles de una categoría
 // Función para mostrar los detalles de una categoría
